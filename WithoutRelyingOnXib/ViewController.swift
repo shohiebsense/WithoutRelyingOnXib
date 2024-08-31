e8fe39e1c7dab984ccb48dc4dd25de5d1cd3c09a
Shohieb <shohiebsense@users.noreply.github.com>
2024-08-31 16:12:38 +0700

    Initial Commit

---
+++ WithoutRelyingOnXib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WithoutRelyingOnXib
+//
+//  Created by user on 2024/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
